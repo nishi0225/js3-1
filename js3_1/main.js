1fb0edfa3915c626812a604490dbacf1cf60c236
nishi0225 <nsrk25@gmail.com>
2020-12-15 16:15:50 +0900

    js課題3-1

--- js3_1/main.js
+++ js3_1/main.js
@@ -1,45 +1,86 @@
 'use strict'
+//入力値,追加Button,表示場所を取得
+const input = document.getElementById('inputValue');
+const addTaskButton = document.getElementById('addTaskButton');
+const addTaskTarget = document.getElementById('addTaskTarget');
+//radioButtoを取得
+const radioWorking = document.getElementById('Working');
+const radioDone = document.getElementById('Done');
 
 //入力値を格納する配列を作成
 const taskList = [];
-//入力値,ボタン,表示場所を取得
-const input = document.getElementsByClassName('inputValue')[0];
-const addTaskButton = document.getElementsByClassName('addTaskButton')[0];
-const addTaskTarget = document.getElementsByClassName('addTaskTarget')[0];
+//index番号を作成
+let tdId = 0;
 
-const addTask = () => {
-  const todos = input.value;
-  taskList.push(todos);
-  const trElement = document.createElement('tr');
-  const tdElementId = document.createElement('td');
-  const tdElementTask = document.createElement('td');
-  for(let i = 0; i < taskList.length; i++) {
-    tdElementId.textContent = i;
-  }
-  tdElementTask.textContent = todos;
-  trElement.appendChild(tdElementId);
-  trElement.appendChild(tdElementTask);
-
-  //状態ボタンを作成
-  const statusButton = document.createElement('button');
-  const tdstatusButton = document.createElement('td');
-  statusButton.textContent = '作業中';
-  tdstatusButton.appendChild(statusButton);
-  //削除ボタンを作成
-  const removeButton = document.createElement('button');
-  const tdremoveButton = document.createElement('td');
-  removeButton.textContent = '削除';
-  tdremoveButton.appendChild(removeButton);
-  //buttonを追加
-  trElement.appendChild(tdstatusButton);
-  trElement.appendChild(tdremoveButton);
-  addTaskTarget.appendChild(trElement);
+//id,taskを作成
+const addTask = (task,id,newTable) => {
+  const tdIdElement = document.createElement('td');
+  const tdTaskElement = document.createElement('td');
+  //taskListに入力値を追加
+  const todo = {
+    task: tdTaskElement,
+    status: '作業中',
+  };
+  taskList.push(todo);
+  //要素内(文字)を作成
+  tdIdElement.textContent = id;
+  tdTaskElement.textContent = task;
+  newTable.appendChild(tdIdElement);
+  newTable.appendChild(tdTaskElement);
+  addTaskTarget.appendChild(newTable);
 }
-//クリック時の処理
-addTaskButton.addEventListener('click', () =>{
-  addTask();
-  input.value = '';
+
+//Buttonを作成
+const addButton = (newTable,removeButton,statusButton) => {
+  const tdstasuButton = document.createElement('td');
+  const tdRemoveButton = document.createElement('td');
+  //Button名を作成
+  statusButton.textContent = '作業中';
+  removeButton.textContent = '削除';
+  //td要素にButtonを追加
+  tdstasuButton.appendChild(statusButton);
+  tdRemoveButton.appendChild(removeButton);
+  //trにtd追加,表示
+  newTable.appendChild(tdstasuButton);
+  newTable.appendChild(tdRemoveButton);
+  addTaskTarget.appendChild(newTable);
+}
+
+//追加Buttonをクリックした際の処理
+addTaskButton.addEventListener('click', () => {
+  const task = inputValue.value;
+  const newTable = document.createElement('tr');
+  const removeButton = document.createElement('button');
+  const statusButton = document.createElement('button');
+
+  addTask(task,tdId++,newTable);
+  addButton(newTable,removeButton,statusButton);
+  inputValue.value = '';
   input.focus();
-})
+
+  //削除Buttonの処理
+  const deleteTask = (deleteTr) => {
+    const tableTr = deleteTr.target.closest('tr');
+    if (tableTr) {
+      tableTr.remove();
+    }
+  }
+  removeButton.addEventListener('click', deleteTask, false);
+
+  //statusButtonの表示切替
+  statusButton.addEventListener('click',(state) => {
+    if (statusButton.textContent === '作業中') {
+      statusButton.textContent = '完了';
+      const doneTask = state.target.parentNode;
+      doneTask.className = 'doneTask';
+    } else {
+      statusButton.textContent = '作業中';
+      const workTask = state.target.parentNode;
+      workTask.className = 'working';
+    }
+  });
+});
+
+
 
 
